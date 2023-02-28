693ac53640985ef4a2965fcea774a4ca4fea05b5
sava911 <pesss929@gmail.com>
2023-02-28 23:00:15 +0300

    start mongoDB

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const PORT = config.get('port') || 5000
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+
+    })
+  } catch (e) {
+    console.log('Server error', e.message)
+    process.exit(1)
+  }
+}
+
+app.listen(PORT, () => console.log(`App has been started ON PORT ${PORT}...`))
